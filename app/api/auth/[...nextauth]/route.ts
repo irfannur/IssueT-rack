ff90a13046fb296df17dfabdd199a41df7495e63
irfannur238 <irfannur238@gmail.com>
2023-11-05 06:20:25 +0700

    route auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,13 +4,16 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
+    adapter: PrismaAdapter(prisma),
     providers: [
-        getAdapter: PrismaAdapter(prisma)
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!
         })
-    ]
+    ],
+    session: {
+        strategy:   "jwt"
+    }
 });
 
 export { handler as GET, handler as POST }
