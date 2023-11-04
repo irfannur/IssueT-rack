886bcbb0f05579a59161ecf94c67c3c5cbeb2cbd
irfannur238 <irfannur238@gmail.com>
2023-11-05 06:19:03 +0700

    add prisma adapter & migrate table user & tbl management auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,11 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/prisma/client";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
     providers: [
+        getAdapter: PrismaAdapter(prisma)
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!
