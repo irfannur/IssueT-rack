2c954e377af19d6100a6d9847442cb61ef4e92a5
irfannur238 <irfannur238@gmail.com>
2023-11-09 21:46:05 +0700

    setup react query devTools (debug)

--- app/QueryClientProvider.tsx
+++ app/QueryClientProvider.tsx
@@ -2,6 +2,7 @@
 
 import React, { PropsWithChildren } from 'react';
 import { QueryClient, QueryClientProvider as ReactQueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'; 
 
 const queryClient = new QueryClient();
 
@@ -9,6 +10,7 @@ const QueryClientProvider = ({ children }: PropsWithChildren) => {
     return (
         <ReactQueryClientProvider client={queryClient}>
             {children}
+            <ReactQueryDevtools />
         </ReactQueryClientProvider>
     )
 }
